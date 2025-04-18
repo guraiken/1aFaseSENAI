377372c0e95b165168e1568e2035e6d025a75562
guraiken <gustavo.fiabani@gmail.com>
2025-04-17 22:11:20 -0300

    Adicionado exercício 3 incompleto (juca)

---
+++ vscode/javascript/Exercicios Rafael/Exercicio 3 juca/script.js
@@ -0,0 +1,19 @@
+// Ler distancia da corrida e tempo de viagem em segundos. caso o tempo de viagem for maior 
+// que 60 segundos fazer em minutos e daí em diante...
+
+function inciarJornada(){
+    let distCorrida = Number(prompt("Digite a distância da corrida: "))
+    let tempoViagem = Number(prompt("Digite o tempo da viagem: "))
+    let resultadoTempo = tempoViagem
+    
+    if(tempoViagem < 60 && tempoViagem >= 0){
+        alert("O tempo de viagem foi:  " + resultadoTempo + " segundo(s).")
+    }else if(tempoViagem >= 60){
+        resultadoTempo = tempoViagem / 60
+        alert("O tempo de viagem foi:  " + resultadoTempo + " minuto(s).")
+    }else if(resultadoTempo >= 60){
+        resultadoTempo = tempoViagem * 60
+        resultadoTempo = resultadoTempo / 24
+        alert("O tempo de viagem foi:  " + resultadoTempo + " hora(s).")
+    }
+}
