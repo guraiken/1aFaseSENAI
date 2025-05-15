bbf3e02327eb328f7d59b04797f25fb28eeaf599
guraiken <gustavo.fiabani@gmail.com>
2025-05-15 13:40:17 -0300

    Exercicios adicionados e ajeitada no css e html para menos linhas

---
+++ vscode/html/Exercicios Alex/DavyJones/Exercicios1.2.3/parte3.js
@@ -0,0 +1,21 @@
+
+//imprima todos os números de 1 a 100 que são pares e múltiplos de 3.
+function exer21(){
+    let numParMult = ''
+    for(let cont = 1; cont <= 100; cont++){
+        if(cont % 3 == 0 || cont % 2 == 0){
+            numParMult = numParMult + ', ' + cont
+        }
+    }
+    alert(numParMult)
+}
+
+//Imprima os números de 1 a 50 que são ímpares ou múltiplos de 7.
+function exer22(){
+
+}
+
+//Mostre todos os números entre 1 e 30 que não são divisíveis por 2 e por 3.
+function exer23(){
+
+}
