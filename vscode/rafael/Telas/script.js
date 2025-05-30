5f504ed65157ae1fef7a5eba97106430afd2b411
guraiken <gustavo.fiabani@gmail.com>
2025-05-29 21:56:26 -0300

    exer att

--- vscode/rafael/Telas/script.js
+++ vscode/rafael/Telas/script.js
@@ -19,3 +19,32 @@ function esconderTodas(){
     document.getElementById("produtos").style.display = "none"
     //esconder novas paginas
 }
+
+// variaveis de nome, email e senha
+// let email, nome, senha
+
+let usuario = {
+    email: '',
+    nome: '',
+    senha: ''
+}
+
+function cadastrar(){
+    usuario.email = document.getElementById("inpCadEmail").value
+    usuario.nome = document.getElementById("inpCadName").value
+    usuario.senha = document.getElementById("inpCadPass").value
+    alert("Usuário cadastrado!")
+    mostrarLogin()
+}
+
+function logar(){
+    let nome = document.getElementById("inpLogNome").value === usuario.nome ? true : false
+    let senha = document.getElementById("inpLogSenha").value === usuario.senha ? true : false
+
+    if(nome == true && senha == true){
+        alert("Login efetuado com sucesso!")
+        mostrarProdutos()
+    }else{ 
+        alert("Login efetuado sem sucesso!")
+    }
+}
