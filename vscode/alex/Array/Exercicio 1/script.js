e6c2f7213423faff3c86b1c7c0bcc32e701eee5d
guraiken <gustavo.fiabani@gmail.com>
2025-06-10 22:17:03 -0300

    .

--- vscode/alex/Array/Exercicio 1/script.js
+++ vscode/alex/Array/Exercicio 1/script.js
@@ -4,10 +4,12 @@ function exer1(){
 
 numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-// Inverte o indice do array pelo tamanho dele
+// Inverte o indice do array pelo tamanho dele (.reverse() funciona desse jeito)
 let numerosinvertidos = numeros.map((elemento, index) =>{
     let tamanhoarray = numeros.length - 1
     return numeros[tamanhoarray - index]
 })
+
+// chaves no map precisa de return pra voltar, ou usar element
 
 console.log(numerosinvertidos)
