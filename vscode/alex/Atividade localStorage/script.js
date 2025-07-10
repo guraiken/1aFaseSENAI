5eadaa571d160e6819f848334134cf903287f128
guraiken <gustavo.fiabani@gmail.com>
2025-07-10 14:26:56 -0300

    resolvendo exercicios de localstorage

---
+++ vscode/alex/Atividade localStorage/script.js
@@ -0,0 +1,126 @@
+let valorSalvo = localStorage.getItem('cont');
+const textarea = document.getElementById('texto');
+
+if (valorSalvo === null) {
+    valorSalvo = 0;
+    localStorage.setItem('cont', valorSalvo);
+} else {
+    valorSalvo = parseInt(valorSalvo);
+}
+
+document.getElementById("contador").innerText = valorSalvo;
+
+function diminuirCont(){
+    let span = document.getElementById("contador");
+    let novoValor = Number(span.innerText) - 1;
+    span.innerText = novoValor;
+    localStorage.setItem("cont", novoValor);
+}
+
+function aumentarCont(){
+    let span = document.getElementById("contador");
+    let novoValor = Number(span.innerText) + 1;
+    span.innerText = novoValor;
+    localStorage.setItem("cont", novoValor);
+}
+
+// carrega o texto salvo quando a página é aberta
+// Pega o valor da chave texto que ta no localStorage
+const textoSalvo = localStorage.getItem('texto');
+
+// Se existir algum texto salvo (se não for nulo), coloca ele dentro do textarea
+if (textoSalvo) {
+    textarea.value = textoSalvo;
+}
+
+// salva o texto toda vez que o usuário digitar algo
+textarea.addEventListener('input', () => {
+    // Salva o valor atual do textarea no localStorage, usando a chave "texto"
+    localStorage.setItem('texto', textarea.value);
+});
+
+//exer 3 não consegui fazer
+
+
+//exer 4
+let textoUsuario = document.getElementById("saudar")
+let usuario  = localStorage.getItem("user")
+if(usuario == null || usuario == undefined){
+    usuario = prompt("Digite seu nome") 
+    localStorage.setItem("user", usuario)
+    usuario = localStorage.getItem("user")
+    textoUsuario.innerHTML = `Saudações, ${usuario}`
+}else{
+    textoUsuario.innerHTML = `Saudações, ${usuario}`
+}
+// exer 4 acaba
+
+//exer 5 
+
+const container = document.getElementById("container")
+//Cria o localStorage
+const corSalva = localStorage.getItem("color")
+
+if(corSalva){
+    container.style.backgroundColor = corSalva;
+}
+
+//funções dos botoes
+function botaoCinza(){
+    let novaCor = container.style.backgroundColor = "rgb(88, 88, 88)"
+    //salva no localStorage
+    localStorage.setItem("color", novaCor)
+}
+function botaoCarmesin(){
+    let novaCor = container.style.backgroundColor = "rgb(196, 54, 54)"
+    localStorage.setItem("color", novaCor)
+}
+function botaoBranco(){
+    let novaCor = container.style.backgroundColor = "white"
+    localStorage.setItem("color", novaCor)
+}
+// exer 6
+
+const botaoTema = document.getElementById("botao-tema")
+
+function aplicarModoNoturno(ativar){
+    if(ativar){
+        container.classList.add("modo-noturno")
+    }else{
+        container.classList.remove("modo-noturno")
+    }
+}
+
+botaoTema.addEventListener("click", ()=> {
+    const estaAtivo = container.classList.contains('modo-noturno')
+    
+    if(estaAtivo){
+        aplicarModoNoturno(false);
+        localStorage.removeItem("modoNoturno")
+    }else{
+        aplicarModoNoturno(true)
+        localStorage.setItem("modoNoturno", "true")
+    }
+})
+
+
+function exer1(){
+    document.getElementById("paginaContador").style.display = "flex"
+}
+function exer2(){
+    document.getElementById("paginaExer2").style.display = "flex"
+}
+function exer5(){
+    document.getElementById("paginaExer5").style.display = "flex"
+}
+function exer6(){
+    document.getElementById("paginaExer6").style.display = "flex"
+}
+
+
+function escondertudo(){
+    document.getElementById("paginaContador").style.display = "none"
+    document.getElementById("paginaExer2").style.display = "none"
+    document.getElementById("paginaExer5").style.display = "none"
+    document.getElementById("paginaExer6").style.display = "none"
+}
